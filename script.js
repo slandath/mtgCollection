a2ce65e4ce7183fa00492cd1db12873dbb120257
slandath <jesus_slanda@hotmail.com>
2022-07-21 19:56:11 -0400

    Cards Heading
    Added Cards heading into list div

--- script.js
+++ script.js
@@ -26,13 +26,13 @@ function getPGCards() {
     fetch(`http://localhost:3000/api/v1/cards`)
     .then((res) => res.json())
     .then((data) => {
-        let output = '<h3>Cards</h3>';
+        let output = '<h3 class="list-group-item list-group-item-dark ">Cards</h3>';
         data.forEach(function(post){
             output += `
             <li class="list-group-item">${post.name}</li>
             `;
         });
-    document.getElementById('output').innerHTML = output;
+    document.getElementById('output-list').innerHTML = output;
     })
     .catch((error) => {
         console.log(error);
@@ -48,13 +48,33 @@ function insertCard() {
      fetch (`http://localhost:3000/api/v1/cards`, {
         method: 'POST',
         body: JSON.stringify({
-            id: `${pussy}`
+            id: "6a058e68-70af-4a64-859c-c881e5578368"
         }),
-        headers: {
+      /*  headers: {
             "Content-type": "application/json; charset=UTF-8"
-        }
+        } */
     })
         .then(res => res.json())
         .then(data => console.log(data));
-};
+    };
 
+// Update Card
+
+/*
+
+fetch(`https://api.scryfall.com/cards/named?exact=${buttfuck}&set=${mouthfuck}`)
+        .then((res) => res.json())
+        .then((data) => {
+            document.getElementById('search-result').innerHTML = "Card Found!"
+            document.getElementById('scryfall-pic').src = data.image_uris.border_crop
+            document.getElementById('scryfall-name').innerHTML = data.name
+            document.getElementById('scryfall-set').innerHTML = data.set_name
+            document.getElementById('scryfall-price').innerHTML = "$" + data.prices.usd
+            document.getElementById('trouble').value = data.id;
+        })
+        .catch((error) => {
+            console.log(error)
+            document.getElementById('search-result').innerHTML = "Card Not Found!";
+        }); 
+
+        */
