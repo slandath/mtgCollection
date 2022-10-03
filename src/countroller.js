793d35cf2144680ddf5e9942b077cc50e99c8f10
Rob Kazirut <robert.kazirut@gmail.com>
2022-10-03 14:55:31 -0400

    Implemented PUT, PATCH, DELETE with data validation

--- src/countroller.js
+++ src/countroller.js
@@ -1,58 +1,117 @@
-const pool = require('../db');
-const queries = require('./queries');
+const pool = require("../db");
+const queries = require("./queries");
 
 const getCards = (req, res) => {
-    pool.query(queries.getCards, (error, results) => {
-        if (error) throw error;
-        res.status(200).json(results.rows);
-    });
+  pool.query(queries.getCards, (error, results) => {
+    if (error) throw error;
+    res.status(200).json(results.rows);
+  });
 };
 
 const findCard = (req, res) => {
-    const name = req.params.name;
+  const name = req.params.name;
 
-    pool.query(queries.findCard, [name], (error, results) => {
-        if (error) throw error;
-        res.status(200).json(results.rows);
-    });
+  pool.query(queries.findCard, [name], (error, results) => {
+    if (error) throw error;
+    res.status(200).json(results.rows);
+  });
 };
 
 const addCard = (req, res) => {
-    const id = req.body.id;
-    const name = req.body.name;
-    const usd = req.body.usd;
+  const id = req.body.id;
+  const name = req.body.name;
+  const usd = req.body.usd;
+  const qty = req.body.quantity;
 
-    pool.query(queries.addCard, [id, name, usd], (error, results) => {
+  pool.query(queries.findCardId, [id], (error, results) => {
+    if (error) throw error;
+
+    if (results.rowCount != 0) {
+      let quantity = results.rows[0].quantity;
+      quantity = parseInt(quantity, 10) + 1;
+
+      pool.query(queries.updateCardQuantity, [quantity, id], (error) => {
+        if (error) throw error;
+        res.status(200).send("Card Quantity Updated!");
+        console.log("Card Quantity Updated");
+      });
+    } else {
+      pool.query(queries.addCard, [id, name, usd, qty], (error) => {
         if (error) throw error;
         res.status(201).send("Card Added!");
-        console.log("Added");
-    })
+        console.log("Card Added");
+      });
+    }
+  });
 };
 
 const removeCard = (req, res) => {
-    const id = req.params.id;
+  const id = req.params.id;
 
-    pool.query(queries.removeCard, [id], (error, results) => {
+  pool.query(queries.findCardId, [id], (error, results) => {
+    if (error) throw error;
+    if (results.rowCount == 0)
+      res.status(400).send(`Card ID: ${id} not found!`);
+    else {
+      pool.query(queries.removeCard, [id], (error) => {
         if (error) throw error;
-        res.status(200).send("Card removed successfully!");
-    });
+        res.status(202).send("Card removed successfully!");
+      });
+    }
+  });
 };
 
 const updateCard = (req, res) => {
-    const { name } = req.body;
-    const { usd } = req.body;
-    const id = req.params.id;
-    
-    pool.query(queries.updateCard, [name, usd, id], (error, results) => {
-        if (error) throw error;
-        res.status(200).send("Card updated!");
-    })
+  const name = req.body.name;
+  const usd = req.body.usd;
+  const id = req.body.id;
+  const quantity = req.body.quantity;
+
+  pool.query(queries.findCardId, [id], (error, results) => {
+    if (error) throw error;
+    if (results.rowCount == 0)
+      res.status(400).send(`Card ID: ${id} not found!`);
+    else {
+      pool.query(
+        queries.updateCard,
+        [name, usd, quantity, id],
+        (error) => {
+          if (error) throw error;
+          res.status(200).send("Card Updated");
+        }
+      );
+    }
+  });
+};
+
+const patchCard = (req, res) => {
+  const id = req.params.id;
+  const name = req.body.name ? req.body.name : null;
+  const usd = req.body.usd ? req.body.usd : null;
+  const quantity = req.body.quantity ? req.body.quantity : null;
+
+  pool.query(queries.findCardId, [id], (error, results) => {
+    if (error) throw error;
+    if (results.rowCount == 0)
+      res.status(400).send(`Card ID: ${id} not found!`);
+      else {
+        pool.query(
+          queries.patchCard,
+          [name, usd, quantity, id],
+          (error) => {
+            if (error) throw error;
+            res.status(200).send("Card Updated");
+          }
+        );
+      }
+  });
 };
 
 module.exports = {
-    getCards, 
-    findCard,
-    addCard,
-    removeCard,
-    updateCard,
+  getCards,
+  findCard,
+  addCard,
+  removeCard,
+  updateCard,
+  patchCard,
 };
--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,13 @@
-const { Router } = require('express');
-const countroller = require('./countroller');
+const { Router } = require("express");
+const countroller = require("./countroller");
 
 const router = Router();
 
-router.get('/', countroller.getCards);
-router.post('/', countroller.addCard);
-router.get('/:name', countroller.findCard);
-router.patch('/:id',countroller.updateCard);
-router.delete('/:id', countroller.removeCard);
-
+router.get("/", countroller.getCards);
+router.post("/", countroller.addCard);
+router.get("/:name", countroller.findCard);
+router.put("/", countroller.updateCard);
+router.delete("/:id", countroller.removeCard);
+router.patch("/:id", countroller.patchCard);
 
 module.exports = router;
